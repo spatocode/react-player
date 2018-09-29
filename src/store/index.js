a932ae1033979856150211dbaf287219498b7e83
Spatocode <impulseofficial069@gmail.com>
2018-09-29 11:16:14 +0100

    subscribe localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,12 @@ import { songs } from '../reducers'
 
 export const store = (
     createStore(
-        combineReducers({ songs })
+        combineReducers({ songs }),
+        (localStorage['redux-store']) ?
+            JSON.parse(localStorage['redux-store']) :
+            {}
     ))
+
+store.subscribe(()=> {
+    localStorage['redux-store'] = JSON.stringify(store.getState())
+})
