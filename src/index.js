8e55c854ecfde65cc238cde4cb831c8c96e9ebd4
Spatocode <impulseofficial069@gmail.com>
2018-09-29 13:00:01 +0100

    Middleware enabled

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { store } from './store'
+import storeFactory from './store'
 import './index.css';
 import { ReactPlayer } from './containers';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = storeFactory()
 
 ReactDOM.render(
     <Provider store={store}>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { songs } from '../reducers'
 
-export const store = (
-    createStore(
-        combineReducers({ songs }),
+const logger = store => next => action => {
+    let result
+    console.groupCollapsed('dispatching', action.type)
+    console.log('prev state', store.getState())
+    console.log('action', action)
+    result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd()
+}
+
+const saver = store => next => action => {
+    let result = next(action)
+    localStorage['redux-store'] = JSON.stringify(store.getState())
+    return result
+}
+
+const storeFactory = () =>
+    applyMiddleware(logger, saver) (createStore) (
+        combineReducers( {songs} ),
         (localStorage['redux-store']) ?
             JSON.parse(localStorage['redux-store']) :
             {}
-    ))
+    )
 
-store.subscribe(()=> {
-    localStorage['redux-store'] = JSON.stringify(store.getState())
-})
+export default storeFactory
